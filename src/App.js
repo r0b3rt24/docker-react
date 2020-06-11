b34c26c848d53300c13a6369a35ce3d3b9dfb871
Han Cao <han_cao@intuit.com>
2020-06-11 18:11:18 -0500

    fnished

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         are you having fun?
         </p>
         <a
           className="App-link"
